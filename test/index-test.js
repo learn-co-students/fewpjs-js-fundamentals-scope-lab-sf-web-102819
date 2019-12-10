71410917bf8a9af86e5db8de20b971d6a77dada8
Allen Shin <geeuho@gmail.com>
2019-12-10 14:10:47 -0800

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('Fix the Scope', function() {
       expect(window.myAnimal()).toEqual('dog')
     })
   })
-
+ 
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
       expect(window.yourAnimal()).toEqual('cat')
